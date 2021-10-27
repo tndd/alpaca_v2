70319d31d01b760243d3fb1a2b422145e577e2b8
tan <ttru702@gmail.com>
2021-10-27 12:07:46 +0900

    change main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from agent import AgentDB, AgentAlpacaApi, TimeFrame
 
 
+def main():
+    # agent_db = AgentDB()
+    agent_alpaca = AgentAlpacaApi()
+    d = agent_alpaca.request_bars_all(
+        timeframe=TimeFrame.DAY_1,
+        symbol='GLD',
+        time_start='2021-10-01',
+        time_end='2021-10-12'
+    )
+    print(d)
+
+
 if __name__ == '__main__':
-    agent_db = AgentDB()
-    # agent_alpaca = AgentAlpacaApi()
-    # d = agent_alpaca.request_bars_all(
-    #     timeframe=TimeFrame.DAY_1,
-    #     symbol='GLD',
-    #     time_start='2021-10-01',
-    #     time_end='2021-10-12'
-    # )
-    # print(d)
+    main()
