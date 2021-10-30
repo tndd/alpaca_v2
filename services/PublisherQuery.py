8c279c02adf0d52f37d70ecef6616475b721ca5f
tan <ttru702@gmail.com>
2021-10-30 15:47:10 +0900

    move sqls into package "services"
    Because sql files are only referenced by the QueryPublisher.
    
    :points of concern:
    absoute import of sql files!

--- services/PublisherQuery.py
+++ services/PublisherQuery.py
@@ -1,4 +1,16 @@
+from enum import Enum
+
+class Order(Enum):
+    CREATE = 'create'
+    SELECT = 'select'
+    INSERT = 'insert'
+
+
 class PublisherQuery:
+    @staticmethod
+    def _get_path(order: Order, filename: str) -> str:
+        return f"./services/sql/{order.value}/{filename}.sql"
+
     @staticmethod
     def _load_query_from_file(path: str) -> str:
         try:
@@ -10,15 +22,15 @@ class PublisherQuery:
 
     @classmethod
     def create_bars(cls) -> str:
-        path = './sql/create/bars.sql'
+        path = cls._get_path(Order.CREATE, 'bars')
         return cls._load_query_from_file(path)
 
     @classmethod
     def insert_bars(cls) -> str:
-        path = './sql/insert/bars.sql'
+        path = cls._get_path(Order.INSERT, 'bars')
         return cls._load_query_from_file(path)
 
     @classmethod
     def select_bars_latest_time(cls) -> str:
-        path = './sql/select/bars_latest_time.sql'
+        path = cls._get_path(Order.INSERT, 'bars_latest_time')
         return cls._load_query_from_file(path)
