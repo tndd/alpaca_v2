083ce1879f2c4f2788c488186c9b98c461e39d85
tan <ttru702@gmail.com>
2021-11-03 14:56:58 +0900

    add blank function test_decision_tree

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from RepositoryBars import RepositoryBars
 from datatypes import TimeFrame
 from datatypes.Symbol import Symbol
+from sklearn.tree import DecisionTreeClassifier
+
+
+def test_decision_tree():
+    pass
 
 
 def main():
     rp_bar = RepositoryBars()
-    # rp_bar.store_bars_all(
-    #     timeframe=TimeFrame.DAY_1,
-    #     time_start='2016-01-01',
-    #     time_end='2021-10-12'
-    # )
     df = rp_bar.get_df_bars(Symbol.AAPL, TimeFrame.DAY_1)
     print(df)
 
