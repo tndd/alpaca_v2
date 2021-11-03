2dcca1d6307b29bf1f160fc6af517dda760b2c97
tan <ttru702@gmail.com>
2021-11-03 21:12:33 +0900

    add column is_price_up_next
    Since we want to use "is_price_up_next"
    as an objectie variable of the decision tree,
    we have the state as true or false.
    
    However, because price movement have three states:
    up, down, and equivalent,
    it is not appropriate to hold the state in true-false values.

--- main.py
+++ main.py
@@ -9,15 +9,28 @@ def test_convert_df():
     rp_bar = RepositoryBars()
     df = rp_bar.get_df_bars(Symbol.AAPL, TimeFrame.DAY_1)
     x = []
-    for r in df.itertuples():
+    state_count = {
+        'eq': 0,
+        'up': 0,
+        'down': 0
+    }
+    for r in df[:-1].itertuples():
         high_bp = ((r.high - r.open) / r.open) * 10000
         low_bp = ((r.low - r.open) / r.open) * 10000
         close_bp = ((r.close - r.open) / r.open) * 10000
-        x.append([high_bp, low_bp, close_bp, r.volume])
+        is_price_up_next = df.loc[r.Index + 1, 'close'] > df.loc[r.Index + 1, 'open']
+        if df.loc[r.Index + 1, 'close'] > df.loc[r.Index + 1, 'open']:
+            state_count['up'] += 1
+        elif df.loc[r.Index + 1, 'close'] < df.loc[r.Index + 1, 'open']:
+            state_count['down'] += 1
+        else:
+            state_count['eq'] += 1
+        x.append([high_bp, low_bp, close_bp, r.volume, is_price_up_next])
+    print(state_count)
     return pd.DataFrame(
         x,
-        columns=['high_bp', 'low_bp', 'close_bp', 'volume'],
-        index=df.time
+        columns=['high_bp', 'low_bp', 'close_bp', 'volume', 'is_price_up_next'],
+        index=df[:-1].time
     )
 
 
