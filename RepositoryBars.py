968332ab40e053c4a9df99b844fdc8123bf83360
tan <ttru702@gmail.com>
2021-11-05 14:49:04 +0900

    add method get_df_bars_relative
    & change scheme
    is_price_up_next -> next_price_movement

--- RepositoryBars.py
+++ RepositoryBars.py
@@ -80,3 +80,38 @@ class RepositoryBars:
             self.cli_db.conn,
             params=(symbol.value, timeframe.value)
         )
+
+    def get_df_bars_relative(self, symbol: Symbol, timeframe: TimeFrame) -> pd.DataFrame:
+        """
+        df scheme of 'df_bars_relative'
+        ---------------------------
+        high_bp: float
+            basis point of price increase from opening price to high price.
+        low_bp: float
+            basis point of price decrease from opening price to low price.
+        close_bp: float
+            basis point of price change from opening price to close price.
+        volume: int
+            trading volume.
+        next_price_movement: int
+            relationship between NEXT time's closing price and opening price.
+            0: down, 1: up, 2: eq
+        """
+        df = self.get_df_bars(symbol, timeframe)
+        x = []
+        for r in df[:-1].itertuples():
+            high_bp = ((r.high - r.open) / r.open) * 10000
+            low_bp = ((r.low - r.open) / r.open) * 10000
+            close_bp = ((r.close - r.open) / r.open) * 10000
+            if df.loc[r.Index + 1, 'close'] > df.loc[r.Index + 1, 'open']:
+                next_price_movement = 1
+            elif df.loc[r.Index + 1, 'close'] < df.loc[r.Index + 1, 'open']:
+                next_price_movement = 0
+            else:
+                next_price_movement = 2
+            x.append([high_bp, low_bp, close_bp, r.volume, next_price_movement])
+        return pd.DataFrame(
+            x,
+            columns=['high_bp', 'low_bp', 'close_bp', 'volume', 'next_price_movement'],
+            index=df[:-1].time
+        )
--- main.py
+++ main.py
@@ -7,31 +7,9 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import train_test_split
 
 
-def test_convert_df():
-    rp_bar = RepositoryBars()
-    df = rp_bar.get_df_bars(Symbol.AAPL, TimeFrame.DAY_1)
-    x = []
-    for r in df[:-1].itertuples():
-        high_bp = ((r.high - r.open) / r.open) * 10000
-        low_bp = ((r.low - r.open) / r.open) * 10000
-        close_bp = ((r.close - r.open) / r.open) * 10000
-        # 0: up, 1: down, 2: eq
-        if df.loc[r.Index + 1, 'close'] > df.loc[r.Index + 1, 'open']:
-            is_price_up_next = 0
-        elif df.loc[r.Index + 1, 'close'] < df.loc[r.Index + 1, 'open']:
-            is_price_up_next = 1
-        else:
-            is_price_up_next = 2
-        x.append([high_bp, low_bp, close_bp, r.volume, is_price_up_next])
-    return pd.DataFrame(
-        x,
-        columns=['high_bp', 'low_bp', 'close_bp', 'volume', 'is_price_up_next'],
-        index=df[:-1].time
-    )
-
-
 def test_decision_tree():
-    df = test_convert_df()
+    rp_bar = RepositoryBars()
+    df = rp_bar.get_df_bars_relative(Symbol.AAPL, TimeFrame.DAY_1)
     df_x = df.drop('is_price_up_next', axis=1)
     df_y = df.is_price_up_next
     (train_x, test_x, train_y, test_y) = train_test_split(df_x, df_y, test_size=0.3, random_state=666)
@@ -65,7 +43,9 @@ def test_store_bars():
 
 
 def main():
-    test_decision_tree()
+    rp_bar = RepositoryBars()
+    df = rp_bar.get_df_bars_relative(Symbol.AAPL, TimeFrame.DAY_1)
+    print(df)
 
 
 if __name__ == '__main__':
